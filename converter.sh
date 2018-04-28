6c9d0ad7179a9c3386fccbf74c56e72eb6f663f2
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-04-28 19:32:58 -0400

    Added functions and Mp3 support
    - some basic code added for moving the video files but that is not
    correct yet so it's commented out

--- converter.sh
+++ converter.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-echo "About to convert any mp4's to mp3 at a256k bitrate "
-for %%a in ("*.mp4") do ffmpeg -i "%%a" -ab 256K -f mp3 "%%~na.mp3"
-pause 
+# TODO: ask user what bitrate they want to user
+
+# TODO: make functions
+##############################################
+# Just move for now ... I can manually delete later ... this is handy just in case I want to keep the videos too
+function moveVideos
+{
 
 
-echo "converting MKV videos to Mp3 at a 256k bitrate "
-for %%a in ("*.mkv") do ffmpeg -i "%%a" -ab 256K -f mp3 "%%~na.mp3"
-pause 
+  # Make a directory to put the videos in
+  mkdir converted$1videos
+
+  # Move the videos - noting that if the file ext is detected, it should already be converted so no "non-converted" video files should be left here
+
+  # Find the files and then move them to the newly created folder
+  find -name "*.$1" -exec mv "*.$1" converted$1videos
+
+  echo "Converted files are now moved"
+  pause
+
+}
+
+function convertVideos
+{
+
+  echo "About to convert any $1 files in the current directly to Mp3s at a 256kbs bitrate "
+  find -name "*.$1" -exec ffmpeg -i {} -acodec libmp3lame -ab 256k {}.mp3 \;
+  pause
+
+  moveVideos $1
+}
+
+
+convertVideos mp4
+convertVideos mkv
+convertVideos webm
 
 
 echo "Done converting"
